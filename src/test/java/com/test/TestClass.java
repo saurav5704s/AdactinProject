af59c584c90c2e55f7efdf4861dee4349af2fc3b
saurav5704s <sauravsingh5704@gmail.com>
2024-04-13 14:15:05 +0530

    Branch push

--- src/test/java/com/test/TestClass.java
+++ src/test/java/com/test/TestClass.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestClass {
@@ -22,7 +23,7 @@ public class TestClass {
 	public static void main(String[] args) throws IOException {
 		
 		
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new EdgeDriver();
 		
 		driver.manage().window().maximize();
 		
